78db479c4db1174f5343148f143df50541874fe4
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-01 02:12:04 -0300

    Iago Antunes Ferreira

---
+++ Curso em Video MySQL/Aula_03.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE 'nome';
+
+create table 'pessoas'{
+	nome varchar(30),
+	udade tinyint(3),
+	sexo char(1),
+	nacionalidade varchar(20)
+	altura float
+	peso float;
+
+}
+
+describe pessoas;
