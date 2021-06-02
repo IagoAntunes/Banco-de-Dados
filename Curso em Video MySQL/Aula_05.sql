77818b4c605e8f33a440d0727cda178ace02e78c
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-02 03:26:21 -0300

    Iago Antunes Ferreira

---
+++ Curso em Video MySQL/Aula_05.sql
@@ -0,0 +1,25 @@
+
+/* Inserindo em pessoas esses valores */ 
+insert into pessoas
+(id,nome,nascimento,sexo,peso,altura,nacionalidade) /* Não precisa colocar chave primaria */
+values
+(default,'Gdodofredo','1984-01-02','M','78,5','1,83','Brasil')
+
+/* Seleciona tudo dentro de 'pessoas' */
+select * from pessoas;
+
+
+insert into pessoas values/* Se a ordem for a mesma nao precisa colocar */
+(default,'Gdodofredo','1984-01-02','M','78,5','1,83','Brasil')
+
+
+
+
+
+
+
+
+
+
+
+
