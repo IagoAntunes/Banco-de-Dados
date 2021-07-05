19265c26767845068102e224c38c6b8c49c22c76
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-05 18:05:13 -0300

    Iago Antunes Ferreira

--- Curso MYSQL/Exercicios/Exercicios aula02.sql
+++ Curso MYSQL/Exercicios/Exercicios aula02.sql
@@ -1,3 +1,4 @@
 /*A equipe de marketing precisa fazer uma pesquisa sobre nomes mais comuns de seus clientes e precisa do
 nome e sobrenome de todos os clientes cadastros.*/
 
+select firstName,LastName from person.person;
