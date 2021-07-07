8e2fa29cef058ea7ea633220e7bfa0107e649dc4
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-06 22:32:32 -0300

    Iago Antunes Ferreira

--- SQL SERVER/GroupBy.sql
+++ SQL SERVER/GroupBy.sql
@@ -1,4 +1,29 @@
 /* GROUP BY */
 
+select MiddleName,count(FirstName) 
+from person.Person
+group by (MiddleName);
 
-select * from 
+
+select * from Sales.SalesOrderDetail;
+
+select productID,avg(OrderQty) 
+from sales.SalesOrderDetail
+group by productID;
+
+select * from Sales.SalesOrderDetail;
+
+
+select top 10 ProductID,sum(LineTotal)
+from sales.SalesOrderDetail
+group by (ProductID)
+order by sum(LineTotal) desc;
+
+
+select * from Production.WorkOrder;
+
+
+select productID,count(productID) as contagem,
+avg(OrderQty) as media
+from production.WorkOrder
+group by productID;
