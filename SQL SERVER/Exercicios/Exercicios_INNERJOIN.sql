3e71a8edc99ae2c826d69a06a771331c2b856330
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-07 14:38:02 -0300

    Iago Antunes Ferreira

--- SQL SERVER/Exercicios/Exercicios_INNERJOIN.sql
+++ SQL SERVER/Exercicios/Exercicios_INNERJOIN.sql
@@ -13,3 +13,20 @@ from person.PersonPhone;
 select PP.BusinessEntityID,PH.Name,PH.PhoneNumberTypeID,PP.PhoneNumber
 from person.PersonPhone PP
 inner join person.PhoneNumberType PH ON PP.PhoneNumberTypeID = PH.PhoneNumberTypeID;
+
+
+
+--AdressId,City,StateProvinceID,Nome do Estado
+--JUNTAR
+
+select top 10 *
+from person.StateProvince
+
+
+select top 10 *
+from person.Address
+
+
+select PA.AddressID,PA.City,PS.StateProvinceID,PS.Name
+from person.StateProvince PS
+INNER JOIN person.Address PA ON PA.StateProvinceID = PS.StateProvinceID ;
