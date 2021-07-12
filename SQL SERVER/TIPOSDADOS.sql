1e5e6f68c43edd1111fd2eef6f3f3fca503c1ad8
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-12 04:09:56 -0300

    Iago Antunes Ferreira

---
+++ SQL SERVER/TIPOSDADOS.sql
@@ -0,0 +1,29 @@
+/* TIPOS DE DADOS 
+
+
+1-Boleanos
+Por pdrao ele é inicializado como nulo e pode receber tanto 1 ou 0
+
+2-Caracteres
+
+Tamanho fixo - char // permite inserir ate uma quantidade fixa de carcterer e sempre ocupa todo o espaço reservado
+
+3-Numeros
+
+TINYINT - Nao tem parte valor freacionado(ex:1.43 , 24.23)
+SMALLINT - mesma coisa so que com limite maior
+INT - mesma coisa so que com limite maior7
+BIGINT - mesa coisaso que com limtie maior
+NUMERIC ou DECIMAL - valores exatos porem permite ter parte fracionados que tampem pode ser especificado a precisão e escala(escala é o numero de digitos na parte fracional) ex: numeric(5,2)7
+
+
+4-Temporais
+
+DATE - Armazea a data no formato AAAA/MM/DD
+DATETIME - armazena data e horas no formato 
+DATETIME2 - data e horas com adiçao de milisegundos
+SMALLDATETIME - data e hora respeitando o limite 
+TIME - horas,minutos,segundos e milisegundos respeitando o limite
+DATETIMEOFSET - permite armazenar informações de date e horas FUSO HORARIO
+
+*/
