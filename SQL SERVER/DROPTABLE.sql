a8b5faa372ff4464f9c128a324b0dca6ce77b75b
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-14 16:12:04 -0300

    Iago Antunes Ferreira

---
+++ SQL SERVER/DROPTABLE.sql
@@ -0,0 +1,28 @@
+/* DROP TABLE */
+
+
+--DROP TABLE 'nomedatabela'
+
+use teste;
+
+drop table dbo.apagar;---Apaga tabela
+
+TRUNCATE TABLE person.password;--Apaga valores tabela
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SQL SERVER/Exercicios/Exercicio_DROP.sql
@@ -0,0 +1,29 @@
+
+
+
+
+CREATE TABLE lapis(
+id int PRIMARY KEY,
+num int NOT NULL
+
+);
+
+SELECT * FROM lapis;
+
+drop table lapis;
+
+INSERT INTO lapis(id,num)
+VALUES 
+(1,17),
+(2,23);
+
+TRUNCATE TABLE lapis;
+
+
+
+
+
+
+
+
+
