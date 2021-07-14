e9b1b36033a72ea0a1858124448e00db7c75c510
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-13 21:34:35 -0300

    Iago Antunes Ferreira

---
+++ URI Exercicios/URI 2741.sql
@@ -0,0 +1,17 @@
+
+/*
+O semestre acabou na Universidade do Sul da Transilvânia. 
+Todos os cursos tiveram suas notas fechadas, apenas a disciplina de Alquimia 104
+ não teve a lista de alunos aprovados.
+
+Portanto, você deverá mostrar a frase 'Approved: ' junto com o nome do aluno e a sua nota
+, para os alunos que foram aprovados (grade ≥7).
+
+Lembre-se de ordenar a lista pela maior nota.
+*/
+
+
+SELECT concat('Approved: ',name),grade
+FROM students
+WHERE grade >= 7
+ORDER BY grade DESC
