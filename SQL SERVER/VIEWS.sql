e6a359350e2097592ee04a985d4abb2e921a0995
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-14 16:47:41 -0300

    Iago Antunes Ferreira

--- SQL SERVER/VIEWS.sql
+++ SQL SERVER/VIEWS.sql
@@ -1,4 +1,15 @@
 /* VIEWS */
 
+/*
+CREATE VIEW [Pessoa Simplificada] AS 
+SELECT FirstName,MiddleName,LastName
+FROM Person.Person
+WHERE TITLE = 'ms';
+*/
 
+CREATE VIEW [teste2] AS 
+SELECT FirstName,MiddleName,LastName,Title
+FROM Person.Person
+WHERE TITLE = 'Ms.';
 
+SELECT * FROM [teste2];
